dac7f253f9dc7038670ce93960bec3f1328c388b
Pradyumna Malladi <pradyumna.malladi2003@gmail.com>
2023-02-17 21:13:48 +0530

    Fixed small error in qI. Used int instead of long int...

--- Lab Sheet 3/qI.c
+++ Lab Sheet 3/qI.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int findHalfwayStep(int sz, int long target){
-    int left = 1;
-    int right = sz;
-    int i;
+int findHalfwayStep(long int sz, long int long target){
+    long int left = 1;
+    long int right = sz;
+    long int i;
     while(left < right){
         i  = (left+right)/2;
         int long sum = sz*(sz-1)/2-i*(i+1)/2+i;
@@ -24,8 +24,10 @@ int main(void){
     long int size;
     scanf("%ld", &size);
     //printf("%ld", size);
+    //Make halfway = [size*(size-1)/4]
     long int halfway = ((size*(size-1))/2); 
-    halfway -= halfway/2;
+    halfway = halfway/2;
+    printf("\n%d\n", halfway);
     printf("%d", findHalfwayStep(size, halfway));
     return 0;
 }
